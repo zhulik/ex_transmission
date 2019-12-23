f7c977bb3535a7c0a976951b9dff51bc94c1575b
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2019-12-23 22:21:58 +0100

    Configure hex, add license and ex_doc

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule Transmission.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      name: "transmission",
+      description: "Elixir client for the Transmission torrent client RPC API",
+      package: package(),
+      source_url: "https://github.com/zhulik/ex_transmission"
     ]
   end
 
@@ -24,7 +28,16 @@ defmodule Transmission.MixProject do
       {:tesla, "~> 1.3"},
       {:poison, "~> 4.0"},
       {:credo, "~> 1.1.5", only: [:dev, :test], runtime: false},
-      {:exactor, "~> 2.2.4", warn_missing: false}
+      {:exactor, "~> 2.2.4", warn_missing: false},
+      {:ex_doc, "~> 0.21.2", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package() do
+    [
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE*),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/zhulik/ex_transmission"}
     ]
   end
 end
